d8543107068130e22f01a2fb0a3fdc497bd89fe9
Huỳnh Chí Hải <47241315+huynhhai4242@users.noreply.github.com>
2021-10-20 15:48:59 +0700

    Find_number.c
    Tìm số vừa nhập trong dãy các số nguyên

---
+++ Find_number.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    system("cls");
+    int max;   
+    printf("Nhap so luong: "); 
+    scanf("%d", &max);
+    int a[max];
+    int so_sanh; 
+    Nhap(&max, &a);
+    Xuat(&max, &a, &so_sanh);
+    getch();
+    return 0;
+}
+
+void Nhap(int* max, int*a) {
+    for (int i = 1; i <= *max; i++)
+    {
+       printf("Nhap so thu %d: ", i); 
+       scanf("%d", &a[i]);
+    }
+}
+
+void Xuat(int* max, int*a, int* so_sanh){
+    printf("Nhap so can tim: ");
+    scanf("%d", &*so_sanh);
+    int vi_tri[*max];
+    int dem=0;
+    for (int i = 1; i <= *max; i++)
+    {
+        if(*so_sanh == a[i])
+        {
+            dem++;
+            vi_tri[dem] = i;
+        }
+    }
+    if(dem > 0){
+        printf("Da tim thay so %d tai vi tri: ", *so_sanh);
+    }
+    for (int i = 1; i <= dem; i++)
+    {
+        if(i==dem)
+        {
+            printf("%d", vi_tri[i]);            
+        }else {
+            printf("%d, ", vi_tri[i]);
+        }   
+    }    
+}
