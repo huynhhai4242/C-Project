dbca6fd18250bba593b49891ec7164c309f3232c
Huỳnh Chí Hải <47241315+huynhhai4242@users.noreply.github.com>
2021-12-01 08:58:05 +0700

    Create Other_B5.c

---
+++ Other_B5.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    system("cls");
+    float s = 0;
+    printf("S(n) = ");
+    for (float i = 0; i < 3; i++)
+    {
+        if (i == 2)
+        {
+            s = s + (1 / (2 * i + 1));
+            printf("1/%.f", (2 * i + 1));
+        }
+        else
+        {
+            s = s + (1 / (2 * i + 1));
+            printf("1/%.f + ", (2 * i + 1));
+        }
+    }
+    printf(" = %.2f", s);
+    return 0;
+}
